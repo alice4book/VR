6d97966667d326137c370e5da383ac6cef2b5a4c
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2024-06-05 20:25:04 +0200

    Added extinguisher particles

---
+++ Assets/GameAssets/Extinguisher/ProduceClouds.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceClouds : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The projectile that's created")]
+    GameObject m_ProjectilePrefab = null;
+
+    [SerializeField]
+    [Tooltip("The point that the project is created")]
+    Transform m_StartPoint = null;
+
+    [SerializeField]
+    [Tooltip("The speed at which the projectile is launched")]
+    float m_LaunchSpeed = 1.0f;
+
+    public void Fire()
+    {
+        GameObject newObject = Instantiate(m_ProjectilePrefab, m_StartPoint.position, m_StartPoint.rotation, m_StartPoint);
+
+        if (newObject.TryGetComponent(out Rigidbody rigidBody))
+            ApplyForce(rigidBody);
+    }
+
+    void ApplyForce(Rigidbody rigidBody)
+    {
+        Vector3 force = m_StartPoint.forward * m_LaunchSpeed;
+        rigidBody.AddForce(force);
+    }
+}
