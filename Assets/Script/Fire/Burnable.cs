61a26c2228bbc9b97886089f292c2614c48f0c52
Klaudia <alice4book@gmail.com>
2024-06-13 03:16:01 +0200

    fix: for build

--- Assets/Script/Fire/Burnable.cs
+++ Assets/Script/Fire/Burnable.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
 
 public class Burnable : MonoBehaviour
 {
