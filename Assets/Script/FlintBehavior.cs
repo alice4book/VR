1c7dfb2cab92a941971301b518d92f11d95d95cd
Klaudia <alice4book@gmail.com>
2024-06-22 16:02:57 +0200

    fix: direction of sparks on flint
    + some objects are static now

--- Assets/Script/FlintBehavior.cs
+++ Assets/Script/FlintBehavior.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -22,7 +24,16 @@ public class FlintBehavior : MonoBehaviour
     private CapsuleCollider _capsuleCollider;
 
     [SerializeField]
+    private BoxCollider _boxCollider;
+
+    [SerializeField]
     private ParticleSystem _particleSystem;
+
+    [SerializeField]
+    private Vector3 _startingPos;
+
+    bool _bIsTriggeredOnce = false;
+
 
     private void Awake()
     {
@@ -36,26 +47,60 @@ public class FlintBehavior : MonoBehaviour
     {
         if (collision.gameObject.tag == "Flint")
         {
-            _triesCount++;
-            _igniteChance = Random.Range(_minChance, 101);
-            if (_igniteChance > 75)
+            _bIsTriggeredOnce = false;
+            _startingPos = HitCalculation(collision, _boxCollider);
+            StartCoroutine(WaitNextFrame(collision));
+        }
+    }
+
+    IEnumerator WaitNextFrame(Collider collision)
+    {
+        yield return 0;
+        _bIsTriggeredOnce = true;
+        _triesCount++;
+        _igniteChance = Random.Range(_minChance, 101);
+        if (_igniteChance > 75)
+        {
+            Vector3 hit = HitCalculation(collision, _boxCollider);
+            _particleSystem.gameObject.transform.position = hit;
+            Vector3 dir = hit - _startingPos;
+            if (dir.z < 0)
             {
-                _particleSystem.Play();
-                _capsuleCollider.enabled = true;
-                Invoke("StopSparks", 0.2f);
+                _particleSystem.gameObject.transform.localRotation = Quaternion.Euler(-90, 0, 0);
             }
-            _minChance += 25;
-            if (_howManyTries < _triesCount)
+            else
             {
-                _minChance = 1;
-                _triesCount = 0;
+                _particleSystem.gameObject.transform.localRotation = Quaternion.Euler(90, 0, 0);
             }
+            _particleSystem.Play();
+            _capsuleCollider.enabled = true;
+            Invoke("StopSparks", 0.2f);
+        }
+        _minChance += 25;
+        if (_howManyTries < _triesCount)
+        {
+            _minChance = 1;
+            _triesCount = 0;
         }
     }
 
     private void StopSparks()
     {
         _capsuleCollider.enabled = false;
+    }
+
+    private Vector3 HitCalculation(Collider a, Collider b)
+    {
+        Vector3 direction;
+        float distance;
+
+        Physics.ComputePenetration(
+            a, a.transform.position, a.transform.rotation,
+            b, b.transform.position, b.transform.rotation,
+            out direction, out distance);
+
+        Vector3 hitPoint = a.transform.position + direction * distance;
+        return hitPoint;
     }
 }
 
