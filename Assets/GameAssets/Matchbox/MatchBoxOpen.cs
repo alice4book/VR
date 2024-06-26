48b4bff27b4a35fbb28ef6ea1d18f5c1fe93e0c0
Klaudia <alice4book@gmail.com>
2024-06-26 23:19:14 +0200

    fix: match box works

--- Assets/GameAssets/Matchbox/MatchBoxOpen.cs
+++ Assets/GameAssets/Matchbox/MatchBoxOpen.cs
@@ -6,20 +6,14 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class MatchBoxOpen : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
-    [SerializeField] private BoxCollider _1boxCollider;
-    [SerializeField] private BoxCollider _2boxCollider;
-    [SerializeField] private BoxCollider _3boxCollider;
-    [SerializeField] private BoxCollider _4boxCollider;
+    [SerializeField] private BoxCollider _boxCollider;
     [SerializeField] private bool _open;
     [SerializeField] private bool _close;
 
     private void Start()
     {
         _animator.enabled = true;
-        _1boxCollider.enabled = false;
-        _2boxCollider.enabled = false;
-        _3boxCollider.enabled = false;
-        _4boxCollider.enabled = false;
+        _boxCollider.enabled = false;
 
     }
 
@@ -33,19 +27,13 @@ public class MatchBoxOpen : MonoBehaviour
 
     private void EnableGrab()
     {
-        _1boxCollider.enabled = true;
-        _2boxCollider.enabled = true;
-        _3boxCollider.enabled = true;
-        _4boxCollider.enabled = true;
+        _boxCollider.enabled = true;
     }
 
     public void CloseBox()
     {
         _open = false;
-        _1boxCollider.enabled = false;
-        _2boxCollider.enabled = false;
-        _3boxCollider.enabled = false;
-        _4boxCollider.enabled = false;
+        _boxCollider.enabled = false;
         _close = true;
         _animator.SetTrigger("Close");
     }
