9ec74f729c8327fe4fbd7aba1c8d4fd0bba9aef8
Klaudia <alice4book@gmail.com>
2024-06-26 17:19:09 +0200

    fix: match better coloring

--- Assets/GameAssets/Matchbox/MatchBox.cs
+++ Assets/GameAssets/Matchbox/MatchBox.cs
@@ -11,7 +11,7 @@ public class MatchBox : MonoBehaviour
     private Coroutine fireCorutine;
 
     [Header("Colors")]
-    [SerializeField] private Material _material;
+    [SerializeField] private Renderer _objectRenderer;
     [SerializeField] private Color _startColor;
     [SerializeField] private Color _endColor;
     [SerializeField] private float _durationOfBurning = 15.0f;
@@ -19,12 +19,24 @@ public class MatchBox : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (!_used && other.gameObject.tag == "MatchBox")
+        if (!_isBurning && !_used && other.gameObject.tag == "MatchBox")
         {
             _match.SetActive(true);
             _used = true;
             fireCorutine = StartCoroutine(FireMatch());
+        }else
+        if (!_used && !_isBurning)
+        {
+            Burnable otherBurnable = other.gameObject.GetComponent<Burnable>();
+            Lighter lighter = other.gameObject.GetComponent<Lighter>();
+            if ((lighter != null && lighter.fireSpawned) || (otherBurnable != null && otherBurnable._isBurning) || (other.gameObject.tag == "FireStarter"))
+            {
+                _match.SetActive(true);
+                _used = true;
+                fireCorutine = StartCoroutine(FireMatch());
+            }
         }
+        else
         if (_isBurning)
         {
             if (other.transform.gameObject.tag == "ExtinguisherClouds")
@@ -42,7 +54,7 @@ public class MatchBox : MonoBehaviour
         while (timeElapsed < _durationOfBurning && _isBurning)
         {
             // Lerp color based on time elapsed
-            _material.color = Color.Lerp(_startColor, _endColor, timeElapsed / _durationOfBurning);
+            _objectRenderer.materials[1].color = Color.Lerp(_startColor, _endColor, timeElapsed / _durationOfBurning);
             // Increment the time elapsed
             timeElapsed += Time.deltaTime;
 
